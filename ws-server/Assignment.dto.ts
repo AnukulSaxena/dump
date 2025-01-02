d27cf835caf7021505ef8788ab99d340df1a0cc7
Anukul Prakash Saxena <101214911+AnukulSaxena@users.noreply.github.com>
2025-01-02 09:04:27 +0530

    Update Assignment.dto.ts

--- ws-server/Assignment.dto.ts
+++ ws-server/Assignment.dto.ts
@@ -1,124 +1,225 @@
-import { Type } from 'class-transformer';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { 
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Pagination,
+} from "@mui/material"; 
 import {
-  IsArray,
-  IsBoolean,
-  IsEnum,
-  IsMongoId,
-  IsNotEmpty,
-  IsObject,
-  IsOptional,
-  IsString,
-  ValidateNested,
-} from 'class-validator';
-import { Types } from 'mongoose';
-import {
-  AttendeesFilterDto,
-  CreateAttendeeDto,
-} from 'src/attendees/dto/attendees.dto';
-import { AssignmentStatus, RecordType } from 'src/schemas/Assignments.schema';
-
-export class AssignmentDto {
-  @IsOptional()
-  @IsString()
-  adminId: string;
-
-  @IsOptional()
-  @IsString()
-  user: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @IsOptional()
-  webinar: string;
-
-  @IsString()
-  @IsNotEmpty()
-  attendee: string;
-
-  @IsString()
-  @IsNotEmpty()
-  recordType: 'preWebinar' | 'postWebinar';
-}
-
-export class preWebinarAssignmentDto {
-  @IsMongoId()
-  webinar: string; // Must be a valid MongoDB ObjectId
-
-  @IsNotEmpty()
-  @ValidateNested()
-  @Type(() => CreateAttendeeDto)
-  attendee: CreateAttendeeDto;
-}
-
-export class GetAssignmentDTO {
-  @IsOptional()
-  @IsObject()
-  @ValidateNested()
-  @Type(() => AttendeesFilterDto)
-  filters: AttendeesFilterDto;
-
-  @IsOptional()
-  @IsString()
-  validCall?: string;
-
-  @IsOptional()
-  @IsMongoId()
-  webinarId?: string;
-
-  @IsEnum(AssignmentStatus, { message: 'assignmentStatus must be a valid value' })
-  assignmentStatus: AssignmentStatus;
-}
-
-export class RequestReAssignmentsDTO {
-  @IsArray()
-  @IsNotEmpty()
-  @IsMongoId({ each: true }) // Validate each item in the array as a MongoID
-  assignments: string[];
-
-  @IsOptional()
-  @IsEnum(['approved', 'rejected'])
-  status: string;
-}
+  clearSuccess,
+  setTabValue as setTab,
+} from "../../features/slices/attendees";
+import { getAttendees } from "../../features/actions/attendees";
+import { attendeeTableColumns } from "../../utils/columnData";
+import { 
+  Visibility, 
+} from "@mui/icons-material";
+import AttendeesFilterModal from "../../components/Attendees/AttendeesFilterModal";
+import ExportWebinarAttendeesModal from "../../components/Export/ExportWebinarAttendeesModal";
+import { resetReAssignSuccess } from "../../features/slices/reAssign.slice";
+import { resetAssignSuccess } from "../../features/slices/assign";
+import PageLimitEditor from "../../components/PageLimitEditor";
+import AttendeesTable from "../../components/Table/AttendeesTable";
+import DataTableContainer from "../../components/Table/DataTableContainer";
 
 
+const WebinarAttendeesPage = (props) => {
+  const {
+    tabValue,
+    page,
+    setPage,
+    subTabValue,
+    selectedRows,
+    setSelectedRows,
+    setIsSelectVisible,
+    setSelectedAssignmentType,
+    selectedAssignmentType,
+  } = props;
 
-class AssignmentAttendee {
-  @IsMongoId()
-  assignmentId: string;
+  const tableHeader = "Attendees Table";
+  const exportExcelModalName = "ExportWebinarAttendeesExcel";
+  const AttendeesFilterModalName = "AttendeesFilterModal";
 
-  @IsMongoId()
-  attendeeId: string;
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-export class ReAssignmentDTO {
+  const { attendeeData, isLoading, isSuccess, totalPages } = useSelector(
+    (state) => state.attendee
+  );
+  const { isSuccess: isSuccessReAssign } = useSelector(
+    (state) => state.reAssign
+  );
+  const { leadTypeData, isSuccess: assignSuccess } = useSelector(
+    (state) => state.assign
+  );
+  const LIMIT = useSelector((state) => state.pageLimits[tableHeader] || 10);
 
-  @IsBoolean()
-  isTemp: boolean;
+  const [filters, setFilters] = useState({});
+  const [selected, setSelected] = useState("All");
 
-  @IsMongoId()
-  employeeId: string;
+  useEffect(() => {
+    if (tabValue !== "enrollments" && subTabValue === "attendees") {
+      dispatch(
+        getAttendees({
+          id,
+          isAttended: tabValue === "postWebinar",
+          page,
+          limit: LIMIT,
+          filters,
+          validCall: selected === "All" ? undefined : selected,
+          assignmentType:
+            selectedAssignmentType === "All"
+              ? undefined
+              : selectedAssignmentType,
+        })
+      );
+    }
+  }, [page, tabValue, LIMIT, filters, selected, selectedAssignmentType]);
 
-  @IsEnum(RecordType)
-  recordType: RecordType;
+  useEffect(() => {
+    if (isSuccess || assignSuccess || isSuccessReAssign) {
+      console.log("isssucess", isSuccess, assignSuccess, isSuccessReAssign);
+      dispatch(
+        getAttendees({
+          id,
+          isAttended: tabValue === "postWebinar",
+          page: 1,
+          limit: LIMIT,
+          filters,
+          validCall: selected === "All" ? undefined : selected,
+          assignmentType:
+            selectedAssignmentType === "All"
+              ? undefined
+              : selectedAssignmentType,
+        })
+      );
+      dispatch(clearSuccess());
+      dispatch(resetReAssignSuccess());
+      dispatch(resetAssignSuccess());
+      setSelectedRows([]);
+    }
+  }, [isSuccess, assignSuccess, isSuccessReAssign]);
 
-  @IsMongoId()
-  webinarId: string;
+  const actionIcons = [
+    {
+      icon: () => (
+        <Visibility className="text-indigo-500 group-hover:text-indigo-600" />
+      ),
+      tooltip: "View Attendee Info",
+      onClick: (item) => {
+        navigate(
+          `/particularContact?email=${item?.email}&attendeeId=${item?._id}`
+        );
+      },
+      readOnly: true,
+    },
+  ];
 
-  @IsArray()
-  @ValidateNested({ each: true }) 
-  @Type(() => AssignmentAttendee) 
-  assignments: AssignmentAttendee[];
-}
+  const AttendeeDropdown = () => {
+    const handleChange = (event) => {
+      const label = event.target.value;
+      setSelected(label);
+      setPage(1);
+    };
 
-export class FetchReAssignmentsDTO{
+    const handleAssignmentChange = (event) => {
+      const label = event.target.value;
+      if (label === "All") {
+        setIsSelectVisible(false);
+        setSelectedRows([]);
+      } else {
+        setIsSelectVisible(true);
+        setSelectedRows([]);
+      }
+      setSelectedAssignmentType(label);
+      setPage(1);
+    };
 
-  @IsMongoId()
-  webinarId?: string;
+    return (
+      <div className="flex gap-4">
+        <FormControl className="w-40 " variant="outlined">
+          <InputLabel id="attendee-label">Activity</InputLabel>
+          <Select
+            labelId="attendee-label"
+            className="h-10"
+            value={selected}
+            onChange={handleChange}
+            label="Activity"
+          >
+            <MenuItem value="All">All</MenuItem>
+            <MenuItem value="Worked">Worked</MenuItem>
+            <MenuItem value="Pending">Pending</MenuItem>
+          </Select>
+        </FormControl>
 
-  @IsEnum(RecordType)
-  recordType: RecordType;
+        <FormControl className="w-40 " variant="outlined">
+          <InputLabel id="attendee-label">Assignment</InputLabel>
+          <Select
+            labelId="attendee-label"
+            className="h-10"
+            value={selectedAssignmentType}
+            onChange={handleAssignmentChange}
+            label="Assignment"
+          >
+            <MenuItem value="All">All</MenuItem>
+            <MenuItem value="Assigned">Assigned</MenuItem>
+            <MenuItem value="Not Assigned"> Not Assigned</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    );
+  };
+  return (
+    <>
+      <DataTableContainer
+        tableHeader={tableHeader}
+        tableUniqueKey="webinarAttendeesTable"
+        ButtonGroup={AttendeeDropdown}
+        filters={filters}
+        totalPages={totalPages}
+        page={page}
+        setPage={setPage}
+        setFilters={setFilters} 
+        filterModalName={AttendeesFilterModalName}
+        exportModalName={exportExcelModalName}
+        isLoading={isLoading}
+      >
+        <AttendeesTable
+          tableData={{
+            columns: attendeeTableColumns,
+            rows: attendeeData.map((row) => ({
+              ...row,
+              leadType: leadTypeData.find((lead) => lead._id === row?.leadType),
+            })),
+          }}
+          actions={actionIcons}
+          page={Number(page) || 1}
+          limit={LIMIT}
+          isLoading={isLoading}
+   
+          selectedRows={selectedRows}
+          setSelectedRows={setSelectedRows}
+        />
+      </DataTableContainer>
 
-  
-  @IsEnum(AssignmentStatus)
-  status: AssignmentStatus;
-}
+      <AttendeesFilterModal
+        modalName={AttendeesFilterModalName}
+        filters={filters}
+        setFilters={setFilters}
+      />
+      <ExportWebinarAttendeesModal
+        modalName={exportExcelModalName}
+        filters={filters}
+        webinarId={id}
+        isAttended={tabValue === "postWebinar" ? true : false}
+      />
+    </>
+  );
+};
+
+
+  export default WebinarAttendeesPage
